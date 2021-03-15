f67ab089ce36cc4e91fd2e8b910a2d6cdd59b2d9
Jahnvi Srivastava <60891187+jahnvisrivastava100@users.noreply.github.com>
2021-03-15 22:11:10 +0530

    Create Contains Duplicate.cpp

---
+++ Contains Duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+bool containsDuplicate(vector<int>& nums) {
+    map<int, bool> myMap;//here creating map with key value of type int and value of bool
+    // unordered_map<int, bool> myMap;
+    for (auto& num: nums) {
+        if (myMap.find(num) != myMap.end())//if we are able to find an element with that key value then we return true
+            return true;
+        else
+            myMap[num] = true;//else if we are not able to find the value for that key we set the value to true(which in a way tell that this has been visited)
+    }
+    return false;//if throughout we are not able to find one which is visited twice we return false
+}
+
+};
