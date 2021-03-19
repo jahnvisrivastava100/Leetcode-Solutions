7748996212f3f7b37010efc786541a3b5da81c8a
Jahnvi Srivastava <60891187+jahnvisrivastava100@users.noreply.github.com>
2021-03-19 12:03:28 +0530

    Update 3sum.cpp

--- 3sum.cpp
+++ 3sum.cpp
@@ -53,3 +53,43 @@ public:
                return res;
 }
 };
+
+//-----optimized-----
+
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+       
+       int low,high,sum; 
+        int n = size(nums);
+        if(n<3)
+        {
+           return {}; 
+        }
+        vector<vector<int>>res;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            low=i+1;
+            high=n-1;
+            sum=0-nums[i];
+            if(i==0||(i>0&&nums[i]!=nums[i-1])){ 
+                while(low<high){
+                  
+                    if(nums[low]+nums[high]==sum){
+                         res.push_back({nums[i],nums[low],nums[high]});
+                        while(low<high && nums[low]==nums[low+1])low++;
+                        while(low<high && nums[high]==nums[high-1])high--;
+                    low++;
+                    high--;
+                        
+                    }
+                    else if(nums[low]+nums[high]<sum)low++;
+                    else {high--;}
+                }
+            }
+     
+    }
+               return res;
+}
+};
