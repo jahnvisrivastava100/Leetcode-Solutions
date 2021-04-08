adfe5fb0f867d27cc69cd12d31739f5291dad97b
Jahnvi Srivastava <60891187+jahnvisrivastava100@users.noreply.github.com>
2021-04-08 21:29:48 +0530

    Create Merge Two Sorted Lists.cpp

---
+++ Merge Two Sorted Lists.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(!l1){
+            return l2;
+        }
+        if(!l2){
+            return l1;
+        }
+        ListNode* s;
+        if(l1->val <= l2->val){
+            s=l1;
+            l1=l1->next;
+        }
+        else{
+            s=l2;
+            l2=l2->next;
+        }
+        ListNode* l3=s;
+        while(l1 && l2){
+            if(l1->val <= l2->val){
+              s->next=l1;
+              s=l1;
+              l1=s->next;
+        }
+            else{
+                s->next = l2;
+                s = l2;
+                l2 = s->next;
+            }
+        }
+        if(!l1){
+            s->next=l2;
+        }
+        else{
+            s->next=l1;
+        }
+        return l3;
+    }
+        
+    
+};
